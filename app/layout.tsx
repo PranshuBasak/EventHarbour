7b17262368b97ced7f83b7574803220893cf1cfd
PranshuBasak <pranshubasak@gmail.com>
2023-12-17 18:27:45 +0530

    Vercel Speed Insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 const poppins = Poppins({ subsets: ['latin'],
 weight: ['400','500','600','700'],
@@ -23,7 +25,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={poppins.variable}>{children}</body>
+        <body className={poppins.variable}>
+          {children}
+          <SpeedInsights />
+        </body>
       </html>
     </ClerkProvider>
   )
