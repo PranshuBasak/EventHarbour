c4dabca0806c33ab550478dce4ae7f63e49bb938
PranshuBasak <pranshubasak@gmail.com>
2023-12-17 23:11:31 +0530

    Create Event Page Created

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -31,11 +31,14 @@ const populateEvent = (query: any) => {
 export async function createEvent({ userId, event, path }: CreateEventParams) {
   try {
     await connectToDatabase()
-
+    
     const organizer = await User.findById(userId)
     if (!organizer) throw new Error('Organizer not found')
 
-    const newEvent = await Event.create({ ...event, category: event.categoryId, organizer: userId })
+    const newEvent = await Event.create({ ...event, 
+      category: event.categoryId,
+      organizer: userId
+      })
     revalidatePath(path)
 
     return JSON.parse(JSON.stringify(newEvent))
